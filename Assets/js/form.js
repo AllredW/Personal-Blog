c5b5f57e8a70a1b51e200305e19448d3f2187ee7
AllredW <allrewil@gmail.com>
2024-07-02 23:08:18 -0400

    overhaul of CSS elements, added light/dark toggle, added navigation buttons, beginning JS code for input forms

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,1 +1,49 @@
-console.log("Hello Form");
+// define variables
+const postForm = document.querySelector("form");
+const nameInput = document.querySelector("#username");
+const titleInput = document.querySelector("#post-title");
+const contentInput = document.querySelector("#post-content");
+
+// function: convert form inputs into an object
+const postSubmit = function (event) {
+  event.preventDefault();
+//   empty fields = alert message
+  if (
+    nameInput.value == "" ||
+    titleInput.value == "" ||
+    contentInput.value == ""
+  ) {
+    alert("Please complete all fields to submit");
+    return;
+  }
+
+  const blogPost = {
+    username: nameInput.value.trim(),
+    title: titleInput.value.trim(),
+    content: contentInput.value.trim(),
+  };
+  console.log(blogPost.username);
+  console.log(blogPost.title);
+  console.log(blogPost.content);
+};
+
+postForm.addEventListener("submit", postSubmit);
+
+// storage array
+const posts = [];
+
+// function: push the object into an array
+const postStore = function (){
+
+
+// push object to array
+    posts.push(blogPost);
+
+    console.log(posts);
+
+        // condenses array into a string
+        localStorage.setItem('posts', JSON.stringify(posts));
+}
+
+
+
